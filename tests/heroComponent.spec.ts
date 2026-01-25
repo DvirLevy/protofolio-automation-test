bf109d75f23cabf037a32dfbde6c6c13bde7f70a
Dvir Levy <dvirlh1@gmail.com>
2026-01-25 21:36:02 +0200

    add test step name to each step

--- tests/heroComponent.spec.ts
+++ tests/heroComponent.spec.ts
@@ -4,16 +4,25 @@ import { Utils } from '../infrastructure/utils';
 import testData from '../infrastructure/testData';
 
 
-test.describe('Hero Component Tests', () => {
-  test('Sample Test - Placeholder', async ({ page }) => {
-    console.log(process.env.HOST);
-    const heroComponent = new HeroComponent(page);
+test('Hero Component Tests', async ({ page }) => {
+  console.log(process.env.HOST);
+  const heroComponent = new HeroComponent(page);
+  test.step("Opening Portfolio Page",async()=>{
     await page.goto('/',{waitUntil:'load'});
     Utils.validateUrl(`${process.env.HOST}`,page)
+  })
+  test.step("verify greeting text present",async ()=>{
     await expect(heroComponent._greetingText).toHaveText(testData.heroPageText.greetingText)
+  })
+  test.step("verify my name is presented",async ()=>{
     await expect(heroComponent._name).toHaveText(testData.heroPageText.titleText)
+  })
+  test.step("verify title with my professional presented",async ()=>{
     await expect(heroComponent._title).toHaveText(testData.heroPageText.professionalTitle)
+  })
+  test.step("verify greeting description text is presented",async ()=>{
     await expect(heroComponent._description).toContainText(testData.heroPageText.descriptionText)
-    await heroComponent.clickLinkedinLink();
-  });
-})
+  })
+  await heroComponent.clickLinkedinLink();
+});
+
